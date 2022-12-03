fe108968053f5c0fff44f44bc54b7e6edb0b3911
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-12-04 00:41:23 +0530

    Add: Test-case To return sad

--- src/test/java/com/moodanalyser/TestMood.java
+++ src/test/java/com/moodanalyser/TestMood.java
@@ -3,8 +3,15 @@ package com.moodanalyser;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMood {
+    MoodAnalyser newMood = new MoodAnalyser();
 
+    @Test
+    public void checkTheMood() {
+        String mood = newMood.analyseMood("I am sad");
+        Assert.assertTrue(mood, true);
+    }
 }
