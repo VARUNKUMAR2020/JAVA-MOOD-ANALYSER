03e93937b12e2e6003feafb647b48f6ec607bd62
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-12-04 00:51:40 +0530

    Refactor: Mood message in constructor

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -2,24 +2,39 @@ package com.moodanalyser;
 
 import java.util.Scanner;
 
-public class MoodAnalyser 
-{
-        static String analyseMood(String checkmood) {
-            if (checkmood.toLowerCase().contains("happy")) {
-                System.out.println("HAPPY MOOD");
-            } else {
-                System.out.println("SAD MOOD");
-            }
-            return "HAPPY";
-        }
+public class MoodAnalyser {
     
-    public static void main( String[] args )
-    {
-        System.out.println( "Mood Analyser" );
+    // CONSTRUCTOR WITHOUT PARAMETER
+    public MoodAnalyser() {
+        Scanner analyse = new Scanner(System.in);
+        System.out.println("ENTER THE MOOD YOUR:  ");
+        String checkmood = analyse.nextLine();
+        if (checkmood.toLowerCase().contains("happy")) {
+            System.out.println("HAPPY MOOD");
+        } else {
+            System.out.println("SAD MOOD");
+        }
+
+    }
+
+    // CONSTRUCTOR WITH PARAMETER
+    public MoodAnalyser(String checkmood) {
+        if (checkmood.toLowerCase().contains("happy")) {
+            System.out.println("HAPPY MOOD");
+        } else {
+            System.out.println("SAD MOOD");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Mood Analyser");
         System.out.println("EXCEPTION HANDLING");
-		Scanner analyse = new Scanner(System.in);
-		System.out.println("ENTER THE MOOD YOUR:  ");
-		String Mood = analyse.nextLine();
-		analyseMood(Mood);
+        Scanner analyse = new Scanner(System.in);
+        System.out.println("ENTER THE MOOD YOUR:  ");
+        String Mood = analyse.nextLine();
+        // CALLING CONSTRUCTOR WITH PARAMETER
+		MoodAnalyser checkmood = new MoodAnalyser(Mood);
+		// CALLING CONSTRUCTOR WITHOUT PARAMETER
+		MoodAnalyser checkMood = new MoodAnalyser();
     }
 }
