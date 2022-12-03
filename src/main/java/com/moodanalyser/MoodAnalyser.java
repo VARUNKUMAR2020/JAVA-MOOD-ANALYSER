52a32084e1e99ef328bdbaa0bca0846cd2926b28
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-12-04 00:36:14 +0530

    Add: Analyse the Mood

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,9 +1,25 @@
 package com.moodanalyser;
 
+import java.util.Scanner;
+
 public class MoodAnalyser 
 {
+        static String analyseMood(String checkmood) {
+            if (checkmood.toLowerCase().contains("happy")) {
+                System.out.println("HAPPY MOOD");
+            } else {
+                System.out.println("SAD MOOD");
+            }
+            return checkmood;
+        }
+    
     public static void main( String[] args )
     {
         System.out.println( "Mood Analyser" );
+        System.out.println("EXCEPTION HANDLING");
+		Scanner analyse = new Scanner(System.in);
+		System.out.println("ENTER THE MOOD YOUR:  ");
+		String Mood = analyse.nextLine();
+		analyseMood(Mood);
     }
 }
---
+++ src/test/java/com/moodanalyser/TestMood.java
@@ -0,0 +1,10 @@
+package com.moodanalyser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMood {
+
+}
